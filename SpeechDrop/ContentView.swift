9265217a6d46856e31db389901f872d14a98a677
Hoye Lam <hoyelam@kin-yee.com>
2025-11-16 15:51:38 +0100

    Initial Commit

---
+++ SpeechDrop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechDrop
+//
+//  Created by Hoye Lam on 16/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechDrop/SpeechDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechDropApp.swift
+//  SpeechDrop
+//
+//  Created by Hoye Lam on 16/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeechDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
